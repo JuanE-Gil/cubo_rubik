cf91fb926619f982c1e6fe80d035e4ae9ceb9b42
Juan E. Gil <JuanE.Gil2002@Gmail.com>
2023-11-07 22:05:14 -0500

    init commit

---
+++ cubo_rubik.py
@@ -0,0 +1,6 @@
+# Cubo Rubik
+
+# Entradas -> tamaño 9 cuadrados por cara, 6 caras, colores.
+# Colores -> Rojo, Verde, Amarillo, Blanco, Azul, Rosado.
+# Proceso -> cuadrados que pueden rotar, arriba, abajo, horario, anti horario .
+# Salida -> pasos para tener el cubo con los colores
